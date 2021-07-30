ea751fc25a8e82a2777d3eb0f9b411cb9fc798f2
Felipe Ribeiro <feliper.dev@gmail.com>
2021-07-30 20:00:45 -0300

    Ajustando main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,4 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pokedex_twitch/app_module.dart';
 import 'package:pokedex_twitch/app_widget.dart';
 
-import 'modules/home/presenter/home.dart';
-
 void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
